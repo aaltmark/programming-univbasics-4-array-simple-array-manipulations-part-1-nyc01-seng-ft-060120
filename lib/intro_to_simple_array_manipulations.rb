9350f1c70752f0a0f2dec8286ebd2dcf18977fa7
Adam Altmark <adamaltmark@gmail.com>
2020-05-12 12:46:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
 def using_push (colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
+
+def using_unshift (boroughs_in_nyc, @new_neighborhood)
+  boroughs_in_nyc.unshift("Tribeca")
+end
